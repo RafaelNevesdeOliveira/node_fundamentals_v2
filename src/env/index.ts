a8bac02430195ebfc0ed6de1ee036dbbe09f6537
RafaelNevesdeOliveira <rafaelneves652@gmail.com>
2024-02-27 10:02:54 -0300

    testes finalizados

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,19 +1,27 @@
-import 'dotenv/config'
+import { config } from 'dotenv'
 import { z } from 'zod'
+
+if (process.env.NODE_ENV === 'test') {
+  config({ path: '.env.test' })
+} else {
+  config()
+}
 
 // QUAL FORMATO DAS VARIAVEIS DE AMBIENTE
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']).default('sqlite'),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3333),
+  PORT: z.coerce.number().default(3333),
 })
 
 // Valida as variáveis de ambiente atuais (process.env) com base no esquema definido, garantindo que elas atendam às expectativas (neste caso, garantindo que DATABASE_URL existe e é uma string). O resultado dessa validação é armazenado em env, que então contém as variáveis de ambiente validadas e prontas para uso no aplicativo.
 const _env = envSchema.safeParse(process.env)
 
 if (_env.success === false) {
-  console.error('🚨 Invalid enviroment variables', _env.error.format)
-  throw new Error('Invalid enviroment variables.')
+  console.error('⚠️ Invalid environment variables', _env.error.format())
+
+  throw new Error('Invalid environment variables.')
 }
 
 export const env = _env.data
